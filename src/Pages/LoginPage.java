347c9c17952df8963c0d0995e3e409519690c26b
Sharmin Hoque <sharminhoque@Sharmins-MBP.home>
2021-12-24 15:17:22 -0500

    added/updated/deleted files

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -4,6 +4,9 @@ public class LoginPage {
 	
 	public static void main (String a []) {
 		System.out.println("Hello LP");
+		String name="admin";
+		String pass = "admin123";
+		System.out.println("Logged in succefull");
 		
 	}
 
